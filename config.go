9b26ed07e898f87a6036df2f7cd5b74711fa0f60
Jordan Dziat <jdziat@ltvco.com>
2024-01-31 19:52:24 -0500

    Test

---
+++ config.go
@@ -0,0 +1,23 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package otlpexporter // import "go.opentelemetry.io/collector/exporter/otlpexporter"
+
+import (
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/configretry"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+// Config defines configuration for OTLP exporter.
+type Config struct {
+	exporterhelper.TimeoutSettings `mapstructure:",squash"`     // squash ensures fields are correctly decoded in embedded struct.
+	QueueConfig                    exporterhelper.QueueSettings `mapstructure:"sending_queue"`
+	RetryConfig                    configretry.BackOffConfig    `mapstructure:"retry_on_failure"`
+	MaxSendSize                    int                          `mapstructure:"max_send_size"`
+	MaxReceiveSize                 int                          `mapstructure:"max_receive_size"`
+	configgrpc.GRPCClientSettings  `mapstructure:",squash"`     // squash ensures fields are correctly decoded in embedded struct.
+}
+
+var _ component.Config = (*Config)(nil)
